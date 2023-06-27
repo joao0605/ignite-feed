789c011eba9d681258b93a20ea922537b9d7878d
João Pedro Sales Rezende Fernandes <joaofernandes0605@gmail.com>
2023-06-27 16:24:22 +0100

    comment style

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://github.com/joao0605.png" alt="" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>João Fernandes</strong>
+                            <time title='23 de junho às 18:00h' dateTime='2023-06-23 18:00'>Cerca de 1h atrás</time>
+
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
